c2c3d005613d06c3df5a4d5a954efe4ee9084dff
Fabriziogianni7 <fabrizio@onbeam.com>
2024-11-28 17:51:48 +0100

    mocks

--- test/mocks/OFTAdapterMock.sol
+++ test/mocks/OFTAdapterMock.sol
@@ -1,30 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { OFTAdapter } from "@layerzerolabs/oft-evm/contracts/OFTAdapter.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {OFTAdapter} from "@layerzerolabs/oft-evm/contracts/OFTAdapter.sol";
+import {BeamOFTAdapter} from "../../contracts/ERC20/BeamOFTAdapter.sol";
 
-contract OFTAdapterMock is OFTAdapter {
-    constructor(
-        address _token,
-        address _lzEndpoint,
-        address _delegate
-    ) OFTAdapter(_token, _lzEndpoint, _delegate) Ownable(_delegate) {}
+contract OFTAdapterMock is BeamOFTAdapter {
+    constructor(address _token, address _lzEndpoint, address _delegate, uint256 _feePercentage)
+        BeamOFTAdapter(_token, _lzEndpoint, _delegate, _feePercentage)
+    {}
 
     // @dev expose internal functions for testing purposes
-    function debit(
-        uint256 _amountToSendLD,
-        uint256 _minAmountToCreditLD,
-        uint32 _dstEid
-    ) public returns (uint256 amountDebitedLD, uint256 amountToCreditLD) {
+    function debit(uint256 _amountToSendLD, uint256 _minAmountToCreditLD, uint32 _dstEid)
+        public
+        returns (uint256 amountDebitedLD, uint256 amountToCreditLD)
+    {
         return _debit(msg.sender, _amountToSendLD, _minAmountToCreditLD, _dstEid);
     }
 
-    function debitView(
-        uint256 _amountToSendLD,
-        uint256 _minAmountToCreditLD,
-        uint32 _dstEid
-    ) public view returns (uint256 amountDebitedLD, uint256 amountToCreditLD) {
+    function debitView(uint256 _amountToSendLD, uint256 _minAmountToCreditLD, uint32 _dstEid)
+        public
+        view
+        returns (uint256 amountDebitedLD, uint256 amountToCreditLD)
+    {
         return _debitView(_amountToSendLD, _minAmountToCreditLD, _dstEid);
     }
 
@@ -35,4 +33,7 @@ contract OFTAdapterMock is OFTAdapter {
     function removeDust(uint256 _amountLD) public view returns (uint256 amountLD) {
         return _removeDust(_amountLD);
     }
+
+    /// @notice just to not count on coverage
+    function test() public view {}
 }
